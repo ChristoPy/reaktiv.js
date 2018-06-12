001052eda0c578b4d08564fd814e8335cb51b4e4
Christopher <christo_campiglia@hotmail.com>
2018-06-12 09:33:59 -0300

    [ADD] Event Creation.

--- Index.js
+++ Index.js
@@ -6,12 +6,10 @@ module.exports = class Reaktiv {
 	constructor (SomeObject) {
 
 		this.__ReaktivInstance__ = new ReaktivInstance (SomeObject);
-
-		return this;
 	}
 
 	On (EventName, PropertyName, CallBackFunction) {
 
-
+		this.__ReaktivInstance__.SetEvent (EventName, PropertyName, CallBackFunction);
 	}
 };
