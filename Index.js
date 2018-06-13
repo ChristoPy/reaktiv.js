07e45b0bffd1125d4d28b1fc0e77d3f30088975b
Christopher <christo_campiglia@hotmail.com>
2018-06-13 20:27:38 -0300

    [ADD] Data Bind.

--- Index.js
+++ Index.js
@@ -6,6 +6,15 @@ module.exports = class Reaktiv {
 	constructor (SomeObject) {
 
 		this.__ReaktivInstance__ = new ReaktivInstance (SomeObject);
+
+
+		return new Proxy (this, {
+
+			set: function (Target, Name, Value) {
+
+				Target.__ReaktivInstance__.__ReaktivData__[Name] = Value;
+			}
+		});
 	}
 
 	On (EventName, PropertyName, CallBackFunction) {
